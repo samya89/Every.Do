c6561d8a8b3dd1bf8973b0a71396621f621909c6
Samya <samya89@gmail.com>
2015-03-25 12:22:16 -0700

    Initial Commit

---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by Samia Al Rahmani on 3/25/15.
+//  Copyright (c) 2015 Samia Al Rahmani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
